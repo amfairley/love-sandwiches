df3dff5b4eb9bed81df1be059cc06f1ba7797b7a
amfairley <adamfairley1990@gmail.com>
2024-05-09 10:22:45 +0100

    function: add while loop to keep asking for input if invalid data

--- run.py
+++ run.py
@@ -25,30 +25,41 @@ def get_sales_data():
     '''
     Get sales figures input from user
     '''
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("For example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("For example: 10,20,30,40,50,60\n")
 
-    data_str = input("Please enter your data here: ")
-    print(f'The data provided is {data_str}')
+        data_str = input("Please enter your data here: ")
 
-    sales_data = data_str.split(",")
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid.")
+            break
+
+    return sales_data
 
 def validate_data(values):
     '''
-    Validates the sales figures input
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     '''
     try:
-        [int(values) for value in values]
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)} values"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.")
+        return False
+    
+    return True
 
 
 
     
-sales_data = get_sales_data()
+data = get_sales_data()
 
